4fd9c33a050d4c974f043084056bb31eb8d4856d
Mark Licup Lumba <108561442+marklumba@users.noreply.github.com>
2023-09-06 10:48:57 +0800

    Add files via upload

---
+++ Channel Advisor Column Consolidation.py
@@ -0,0 +1,130 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox, ttk
+import pandas as pd
+import os
+import numpy as np
+import re
+import xlwings as xw
+
+
+# Declare df as a global variable
+df = None
+
+# initalise the tkinter GUI
+root = tk.Tk()
+root.title("Channel Advisor Column Consolidation")
+
+root.geometry("600x600") # set the root dimensions
+root.pack_propagate(False) # tells the root to not let the widgets inside it determine its size.
+root.resizable(0, 0) # makes the root window fixed in size.
+
+# Frame for TreeView
+frame1 = tk.LabelFrame(root, text="Excel Data")
+frame1.place(height=300, width=600)
+
+# Frame for open file dialog
+file_frame = tk.LabelFrame(root, text="Open File")
+file_frame.place(height=200, width=600, rely=0.65, relx=0)
+
+# Buttons
+button1 = tk.Button(file_frame, text="Browse for File", command=lambda: File_dialog())
+button1.place(rely=0.2, relx=0.01)
+
+button2 = tk.Button(file_frame, text="Run Transformation", command=lambda: Transform_1())
+button2.place(rely=0.4, relx=0.01)
+
+# The file/file path text
+label_file = ttk.Label(file_frame, text="No File Selected")
+label_file.place(rely=0, relx=0)
+
+# Treeview Widget
+tv1 = ttk.Treeview(frame1)
+tv1.place(relheight=1, relwidth=1) # set the height and width of the widget to 100% of its container (frame1).
+
+treescrolly = tk.Scrollbar(frame1, orient="vertical", command=tv1.yview) # command means update the yaxis view of the widget
+treescrollx = tk.Scrollbar(frame1, orient="horizontal", command=tv1.xview) # command means update the xaxis view of the widget
+tv1.configure(xscrollcommand=treescrollx.set, yscrollcommand=treescrolly.set) # assign the scrollbars to the Treeview Widget
+treescrollx.pack(side="bottom", fill="x") # make the scrollbar fill the x axis of the Treeview widget
+treescrolly.pack(side="right", fill="y") # make the scrollbar fill the y axis of the Treeview widget
+
+def File_dialog():
+    """This Function will open the file explorer and assign the chosen file path to label_file"""
+    filename = filedialog.askopenfilename(initialdir="/",
+                                          title="Select A File",
+                                          filetype=(("xlsx files", "*.xlsx"),("All Files", "*.*")))
+    label_file["text"] = filename
+    return None
+
+def Transform_1():
+    """If the file selected is valid this will load the file into the Treeview"""
+    global df  # Declare df as global to update it
+    file_path = label_file["text"]
+    try:
+        excel_filename = r"{}".format(file_path)
+        if excel_filename[-4:] == ".csv":
+            df = pd.read_csv(excel_filename)
+        else:
+            df = pd.read_excel(excel_filename)
+
+            # Create a mapping of AttributeValue to AttributeName
+            mapping = {}
+            for col in df.columns:
+                if col.endswith("Value"):
+                   prefix = col.split("Value")[0]
+                   value_col = f"{prefix}Name"
+                   if value_col in df.columns:
+                      mapping[col] = df.at[0, value_col]
+            
+        
+            # Rename columns based on the mapping
+            df.rename(columns=mapping, inplace=True)
+            
+            # Drop the AttributeName columns
+            df = df.loc[:, ~df.columns.str.contains('Attribute.*Name')]
+
+            # Export the transformed data to a new Excel file
+            file_path = os.path.join(os.path.expanduser("~"), "Desktop", "Channel Advisor Column Consolidation.xlsx")
+            df.to_excel(file_path, index=False, freeze_panes=(1, 0))
+
+            # Open the Excel file and set all columns width to 15
+            with xw.App(visible=False) as app:
+               wb = xw.Book(file_path)
+
+               # Loop through all worksheets in the workbook
+               for ws in wb.sheets:
+                   # Loop through all columns in the worksheet
+                   for column in ws.api.UsedRange.Columns:
+                       column.ColumnWidth = 15
+
+               # Save the workbook if needed
+               wb.save()
+
+               # Close the workbook
+               wb.close()
+    
+    except ValueError:
+        tk.messagebox.showerror("Information", "The file you have chosen is invalid")
+        return None
+    except FileNotFoundError:
+        tk.messagebox.showerror("Information", f"No such file as {file_path}")
+        return None
+
+    clear_data()
+    tv1["column"] = list(df.columns)
+    tv1["show"] = "headings"
+    for column in tv1["columns"]:
+        tv1.heading(column, text=column) # let the column heading = column name
+
+    df_rows = df.to_numpy().tolist() # turns the dataframe into a list of lists
+    for row in df_rows:
+        tv1.insert("", "end", values=row) # inserts each list into the treeview. For parameters see https://docs.python.org/3/library/tkinter.ttk.html#tkinter.ttk.Treeview.insert
+    return None
+    
+def clear_data():
+    tv1.delete(*tv1.get_children())
+    return None
+
+root.mainloop()
+
+
+
